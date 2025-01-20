46db774607d7e7fea5ec76b496615217e16e46c6
Anand Singh <anand.singh@gmail.com>
2025-01-21 03:18:50 +0530

    All files added

--- src/ex_21092025/Lab006.java
+++ src/ex_21092025/Lab006.java
@@ -2,6 +2,6 @@ package ex_21092025;
 
 public class Lab006 {
     public static void main(String[] args) {
-        System.out.println("add git");
+        System.out.println("|add git|");
     }
 }
