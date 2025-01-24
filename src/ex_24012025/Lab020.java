c504184f4ae07168fcfe57a8c8b60210907b7dd2
Anand Singh <mailto:anand.singh@gmail.com>
2025-01-24 12:50:51 +0530

    All files added

--- src/ex_24012025/Lab020.java
+++ src/ex_24012025/Lab020.java
@@ -2,4 +2,28 @@ package ex_24012025;
 
 public class Lab020 {
 
+    public static void main(String[] args) {
+
+        // Operators
+        int age = 30;
+        // '=' operator assign value literal to the reference to data type
+
+        // Arithematic Operator +,-,*,/
+        // % - modulus
+
+        int a = 15;
+        int b = 25;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+
+        System.out.println(a%b); // remainder
+
+
+
+        System.out.println();
+    }
+
 }
---
+++ src/ex_24012025/Lab021.java
@@ -0,0 +1,16 @@
+package ex_24012025;
+
+public class Lab021 {
+    public static void main(String[] args) {
+
+        // Urinary Operator
+
+        int age = 30;
+        int car = -1;
+
+        System.out.println(age);
+        System.out.println(car);
+
+//        '+' positive sign not need to be given before int, it itself think as +ve value
+    }
+}
