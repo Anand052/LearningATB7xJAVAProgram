e72079d5f415fbae989bbf738eefa7241db1a7c2
Anand Singh <anand.singvcare@gmail.com>
2025-02-03 20:11:27 +0530

    Updated all files!!

--- src/ex30012025/Lab096.java
+++ src/ex30012025/Lab096.java
@@ -3,10 +3,10 @@ package ex30012025;
 public class Lab096 {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Anand"); // 0-9
+            System.out.println("anand"); // 0-9
         }
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Singh"); // 1-10
+            System.out.println("singh"); // 1-10
         }
     }
 }
